c3e1875da79818bcf4a13232bca623adf10d6de5
alanfg <aherrera@asesoftware.com>
2024-09-21 12:00:55 -0500

    dialogo marker

--- mapacacias/src/App.tsx
+++ mapacacias/src/App.tsx
@@ -1,8 +1,27 @@
-import { Heading, Input, InputGroup, InputRightElement, Select } from "@chakra-ui/react";
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogCloseButton,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+  Button,
+  Heading,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Select,
+  useDisclosure,
+} from "@chakra-ui/react";
 import "./App.css";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, TileLayer } from "react-leaflet";
 import { SearchIcon } from "@chakra-ui/icons";
+import { useRef } from "react";
 function App() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const cancelRef = useRef(null);
+
   return (
     <div className="container">
       <section className="menu">
@@ -18,7 +37,7 @@ function App() {
           <InputGroup>
             <Input placeholder="Buscar" backgroundColor="white" />
             <InputRightElement>
-              <SearchIcon/>
+              <SearchIcon />
             </InputRightElement>
           </InputGroup>
         </div>
@@ -31,13 +50,37 @@ function App() {
             attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
           />
-          <Marker position={[3.9871, -73.7649]}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
+          <Marker
+            position={[3.9871, -73.7649]}
+            eventHandlers={{ click: onOpen }}
+          ></Marker>
         </MapContainer>
       </section>
+      <AlertDialog
+        motionPreset="slideInBottom"
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+        isOpen={isOpen}
+        isCentered
+      >
+        <AlertDialogOverlay />
+        <AlertDialogContent>
+          <AlertDialogHeader>Discard Changes?</AlertDialogHeader>
+          <AlertDialogCloseButton />
+          <AlertDialogBody>
+            Are you sure you want to discard all of your notes? 44 words will be
+            deleted.
+          </AlertDialogBody>
+          <AlertDialogFooter>
+            <Button ref={cancelRef} onClick={onClose}>
+              No
+            </Button>
+            <Button colorScheme="red" ml={3}>
+              Yes
+            </Button>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </div>
   );
 }
